3bb5e991cf21dd683da2be2e0c3f766213eb5f74
Ashmit Verma <a1verma8105@gmail.com>
2024-03-28 00:14:25 +0530

    tested api

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
@@ -6,14 +6,14 @@ const websocketHelper = require('./helpers/websocketsHelper');
 const { connectionHandler } = require('./handlers/websocketHandler');
 const khokhaEntryRouter = require('./routers/khokhaEntryRouter');
 const securityKeyMiddleware = require('./middlewares/securityKeyMiddleware');
-
+const mongoose=require("mongoose");
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 // TODO: Add User Auth Middleware
 app.use(express.json());
-app.use(securityKeyMiddleware);
+//app.use(securityKeyMiddleware);
 app.use('/', khokhaEntryRouter);
 
 wss.on('connection', connectionHandler);
@@ -26,6 +26,16 @@ exports.onKhokhaEntryAdded = (connectionId, data) => {
     });
 }
 
-server.listen(process.env.PORT, () => {
+exports.onkhokhaEntryClosed = (connectionId,data)=>{
+    wss.clients.forEach((client)=>{
+        if(client.connectionId == connectionId)
+        {
+            websocketHelper.sendMessageToClient(client, data);
+        }
+    })
+}
+
+server.listen(process.env.PORT, async() => {
+    await mongoose.connect(process.env.DATABASE_URI);
     console.log(`Server running on port ${process.env.PORT}`);
 });
