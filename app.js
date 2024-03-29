5919a7638adf8f63e23af73b94fc86bacb18f1f6
Hardik Roongta <hardikroongta8@gmail.com>
2024-03-30 03:26:25 +0530

    change

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 });
 app.use(express.json());
 app.use(securityKeyMiddleware);
-app.use('/khokhaEntry/api', khokhaEntryRouter);
+app.use('/test/khokhaEntry/api', khokhaEntryRouter);
 app.use(errorHandler);
 
 wss.on('connection', connectionHandler);
