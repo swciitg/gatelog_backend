7d21946e0369121f33e1854439ef675b8c9fa333
Hardik Roongta <hardikroongta8@gmail.com>
2024-03-30 23:32:51 +0530

    change

--- helpers/onestopUserHelper.js
+++ helpers/onestopUserHelper.js
@@ -4,10 +4,10 @@ const { onestopUserEndpoint } = require("../shared/constants");
 exports.getOnestopUser = async(authHeader, onestopSecurityKey) => {
     const res = await axios.get(onestopUserEndpoint, {
         headers: {
-            "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI2NGM3NWM4YWE2ZDE3ZWE0NTYyM2M5ODIiLCJpYXQiOjE3MTE0MjkxMjIsImV4cCI6MTcxMjI5MzEyMn0.VvDrXXH6duBVVopthvutOGyUxW_w7JI5BfObBBoAII4",
-            // "authorization": authHeader,
-            "security-key": "OneStop-Test"
-            // "security-key": onestopSecurityKey,
+            // "authorization": "Bearer ",
+            "authorization": authHeader,
+            // "security-key": "OneStop-Test"
+            "security-key": onestopSecurityKey,
         },
     });
     
