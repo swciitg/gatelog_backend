c310428bbaf4bbb4fb299e762ed0d961601e11b2
Ashmit Verma <a1verma8105@gmail.com>
2024-05-12 21:56:52 +0530

    pagination done

--- controllers/khokhaHistoryController.js
+++ controllers/khokhaHistoryController.js
@@ -1,17 +1,36 @@
-import KhokhaEntryModel from '../models/KhokhaEntryModel.js'
+import KhokhaEntryModel from '../models/KhokhaEntryModel.js';
+import { getOnestopUser } from '../helpers/onestopUserHelper.js';
 
 export const khokhaHistoryController = {
     userHistory: async (req, res, _next) => {
-        const rollNo = req.params.rollNo;
         try {
-            const history = await KhokhaEntryModel.findOne({rollNo: rollNo});
-            if (!history) {
-                return res.status(404).json({message: "No History"});
+            const { user } = await getOnestopUser(req.headers.authorization, req.headers["security-key"]);
+            const onestopUser = user.outlookEmail;
+
+
+            let { page, size, sort } = req.query;
+            if (!page) page = 1;
+            if (!size) size = 10;
+            const limit = parseInt(size);
+            const skip = (parseInt(page) - 1) * limit;
+            const history = await KhokhaEntryModel.find({ outlookEmail: onestopUser })
+                                                .sort({ createdAt: -1 })
+                                                .skip(skip)
+                                                .limit(limit);
+
+            if (history.length === 0) {
+                return res.status(404).json({ message: "No History" });
             } else {
-                return res.status(200).json(history);
+                return res.status(200).send({
+                    page: parseInt(page),
+                    size: limit,
+                    totalPages: Math.ceil(history.length / limit),
+                    history
+                });
             }
         } catch (error) {
-            return res.status(500).json({message: "Internal Server error"});
+            console.error("Error:", error);
+            return res.status(500).json({ message: "Internal Server error" });
         }
     }
 }
