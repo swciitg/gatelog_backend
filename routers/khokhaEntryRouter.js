04013ee6ae6193f0538f17cbaa66fc675e954ac2
Hardik Roongta <hardikroongta8@gmail.com>
2024-06-06 11:45:56 +0530

    minor change

--- routers/khokhaEntryRouter.js
+++ routers/khokhaEntryRouter.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import {khokhaController} from '../controllers/khokhaEntryController.js';
 import {khokhaHistoryController} from '../controllers/khokhaHistoryController.js';
-import {getUserInfo} from "../middlewares/getUserInfo.js";
+import {getUserInfo, verifyAuthentication} from "../middlewares/getUserInfo.js";
 
 const khokhaEntryRouter = express.Router();
 
 khokhaEntryRouter.post("/newEntry", getUserInfo, khokhaController.addNewEntry);
 khokhaEntryRouter.patch("/closeEntry/:id", khokhaController.closeEntry);
 
-khokhaEntryRouter.get("/history", getUserInfo, khokhaHistoryController.userHistory);
+khokhaEntryRouter.get("/history", verifyAuthentication, khokhaHistoryController.userHistory);
 
 export default khokhaEntryRouter;
