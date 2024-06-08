b33f5b22f07e46f844d423ce96a1ed9ab9827f24
Hardik Roongta <hardikroongta8@gmail.com>
2024-06-08 20:17:12 +0530

    minor change

--- controllers/khokhaHistoryController.js
+++ controllers/khokhaHistoryController.js
@@ -3,14 +3,12 @@ import KhokhaEntryModel from "../models/KhokhaEntryModel.js";
 export const khokhaHistoryController = {
   userHistory: async (req, res, _next) => {
     try {
-      const onestopUser = req.user.outlookEmail;
-
-      let { page, size, sort } = req.query;
+      let { page, size } = req.query;
       if (!page) page = 1;
       if (!size) size = 10;
       const limit = parseInt(size);
       const skip = (parseInt(page) - 1) * limit;
-      const history = await KhokhaEntryModel.find({ outlookEmail: onestopUser })
+      const history = await KhokhaEntryModel.find({ outlookEmail: req.user.outlookEmail })
         .sort({ createdAt: -1 })
         .skip(skip)
         .limit(limit);
@@ -21,7 +19,6 @@ export const khokhaHistoryController = {
         return res.status(200).send({
           page: parseInt(page),
           size: limit,
-          totalPages: Math.ceil(history.length / limit),
           history,
         });
       }
