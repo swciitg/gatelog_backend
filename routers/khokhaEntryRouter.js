b53d588cee0e71699ff493b31f1548615a6619e1
swciitg <swciitghy@gmail.com>
2025-03-10 19:04:28 +0530

    adds error handler

--- routers/khokhaEntryRouter.js
+++ routers/khokhaEntryRouter.js
@@ -3,16 +3,17 @@ import {khokhaController} from '../controllers/khokhaEntryController.js';
 import {khokhaHistoryController} from '../controllers/khokhaHistoryController.js';
 import {getUserInfo, verifyUserInfo} from "../middlewares/getUserInfo.js";
 import securityKeyMiddleware from "../middlewares/securityKeyMiddleware.js";
+import asyncErrorHandler from '../handlers/asyncErrorHandler.js';
 
 const khokhaEntryRouter = express.Router();
 
 khokhaEntryRouter.post("/newEntry", securityKeyMiddleware, 
-    getUserInfo, khokhaController.addNewEntry
+    getUserInfo, asyncErrorHandler(khokhaController.addNewEntry)
 );
 khokhaEntryRouter.patch("/closeEntry/:id", 
-    securityKeyMiddleware, khokhaController.closeEntry
+    securityKeyMiddleware, asyncErrorHandler(khokhaController.closeEntry)
 );
 
-khokhaEntryRouter.get("/history", verifyUserInfo, khokhaHistoryController.userHistory);
+khokhaEntryRouter.get("/history", verifyUserInfo, asyncErrorHandler(khokhaHistoryController.userHistory));
 
 export default khokhaEntryRouter;
